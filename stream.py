606afa55e227125f57ababa761195702418c208d
delimarr <robin.delimar@students.fhnw.ch>
2024-02-14 12:49:06 +0100

    -

--- stream.py
+++ stream.py
@@ -4,8 +4,16 @@ from threading import Thread
 from replay_converter import ReplayConverter
 from ecos_converter import EcosConverter
 
+# dab lap
 folder = "./data/got_raw_files/run8/"
 files = ["02_dab"]
+
+# measure trainswitch
+folder_ts = "./data/got_raw_files/run5_ts/"
+files_ts = [
+    ["dab_ew01_dcc019"],
+    ["dab_ew05_dcc014"]
+]
 
 if not os.path.exists(os.path.join(folder, files[0])):
     raise ValueError("you're an donkey.")
@@ -14,7 +22,7 @@ if not os.path.exists(os.path.join(folder, files[0])):
 def replay_gtcommand():
     """Replay raw got files to socket."""
     rp = ReplayConverter(
-        files=files, folder=folder, raw_flg=True, fixed_intervall=0.001
+        files=files, folder=folder, raw_flg=True, fixed_intervall=0.05
     )
     rp.read_input()
 
